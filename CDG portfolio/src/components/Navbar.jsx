d1fddae9821f0b39b0cba572ca41ca5bff533cc8
SamFems <samueloluwaferanmi37@gmail.com>
2024-12-28 04:29:24 +0100

    Continue

--- CDG portfolio/src/components/Navbar.jsx
+++ CDG portfolio/src/components/Navbar.jsx
@@ -4,15 +4,15 @@ const Navbar = () => {
   return (
     <div className='nav-bar'>
       <h1 className='logo'>C . D . G</h1>
-     <ul>
-    <li><a href="#Home">Home</a></li>
-    <li><a href="#About">About</a></li>
-    <li><a href="#Services">Services</a></li>
-    <li><a href="#Projects">Projects</a></li>
-     </ul>
-     <div className="btns">
-      <button className='btns'>Contact</button>
-     </div>
+      <ul>
+        <li><a href="#Home">Home</a></li>
+        <li><a href="#About">About</a></li>
+        <li><a href="#Services">Services</a></li>
+        <li><a href="#Projects">Projects</a></li>
+      </ul>
+      <div className="btns">
+        <button className='btns'>Contact</button>
+      </div>
     </div>
   )
 }
